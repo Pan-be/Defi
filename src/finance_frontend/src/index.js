d82466a3a4346b82ea93c2935dc1376b24b16959
Konrad <konradbieniek@gmail.com>
2023-05-14 15:19:09 +0200

    Initial commit.

---
+++ src/finance_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { finance_backend } from "../../declarations/finance_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await finance_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
